19c0b04324703ff6641fc5bd5a8710b43b2a9cb8
marks214 <aimee.oz.x@gmail.com>
2020-11-17 18:55:12 -0800

    updated routes and application view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Omniauth Login Route
+  get "/auth/github", as: "github_login"
+
+  # Omniauth Github callback route
+  get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
+
   root "works#root"
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
+
+  delete "/logout", to: "users#destroy", as: "logout"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
