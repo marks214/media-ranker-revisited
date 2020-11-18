806c8f5278e7aee97d274395f335edcd4560f66a
marks214 <aimee.oz.x@gmail.com>
2020-11-17 22:15:54 -0800

    added login/logout tests to the users controller test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,68 @@
-require "test_helper"
+require 'test_helper'
 
 describe UsersController do
+
+  describe 'login' do
+    it 'can log in an existing user' do
+      user = perform_login(users(:ada))
+
+      must_respond_with :redirect
+    end
+
+    it 'can log in a new user' do
+      new_user = User.new(
+        username: 'Hello World',
+        provider: 'github',
+        email: 'hw@hw.net',
+        uid: '1234',
+        name: 'Very Serious'
+                          )
+
+      expect {
+        logged_in_user = perform_login(new_user)
+      }.must_change 'User.count', 1
+    end
+
+    describe 'current' do
+      it 'returns 200 OK for a logged-in user' do
+        # Arrange
+        perform_login
+
+        # Act
+        # TODO: check this path, chris had current_user_path,
+        # this should be users/:id? or:
+        get users_path
+        #get github_login_path
+        #get omniauth_callback_path
+
+        # Assert
+        must_respond_with :success
+      end
+    end
+
+    describe "logout" do
+      it "can logout an existing user" do
+
+        # Arrange
+        perform_login
+
+        expect(session[:user_id]).wont_be_nil
+
+        delete logout_path, params: {}
+
+        expect(session[:user_id]).must_be_nil
+        must_respond_with :redirect
+        must_redirect_to root_path
+
+      end
+    end
+
+    it "guest users on that route" do
+
+    end
+
+    it "guest user tries to log out" do
+
+    end
+  end
 end
